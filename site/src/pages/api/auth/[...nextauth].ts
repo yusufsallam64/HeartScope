94950cdea0b337ff5b414e2de60bf4714af79fa1
Yusuf Sallam <68395788+yusufsallam64@users.noreply.github.com>
2025-02-22 18:54:46 -0500

    Remove extraneous JWT env dependency

--- site/src/pages/api/auth/[...nextauth].ts
+++ site/src/pages/api/auth/[...nextauth].ts
@@ -8,8 +8,7 @@ import { DatabaseService } from "@/lib/db/service";
 
 if (
   !process.env.GOOGLE_CLIENT_ID ||
-  !process.env.GOOGLE_CLIENT_SECRET ||
-  !process.env.JWT_SECRET
+  !process.env.GOOGLE_CLIENT_SECRET
 ) {
   throw new Error("Missing required environment variables");
 }
